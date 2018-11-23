ecac0880377fcbcbdd34fe48e812cb7bf34944d9
espressom <45090300+espressom@users.noreply.github.com>
2018-11-23 15:26:58 +0900

    WEEK 1_NewAccountManager_YU

---
+++ New AccountManager/src/new_account_manager_1/Main.java
@@ -0,0 +1,14 @@
+package new_account_manager_1;
+
+public class Main {
+
+	public static final int WIDTH_SIZE = 700;
+	public static final int HEIGHT_SIZE = 600;
+	
+	public static void main(String[] args) {
+		
+		new NewAccountManager();
+		
+	}
+
+}
---
+++ New AccountManager/src/new_account_manager_1/NewAccountManager.java
@@ -0,0 +1,17 @@
+package new_account_manager_1;
+
+import javax.swing.JFrame;
+
+public class NewAccountManager extends JFrame {
+	
+	public NewAccountManager() {
+		
+		setTitle("New Account Manager");
+		setSize(Main.WIDTH_SIZE, Main.HEIGHT_SIZE);
+		setResizable(false);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+
+}
