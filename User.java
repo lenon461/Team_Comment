138cb4de5cfa436f8962463d94bf5cc70e46d31c
espressom <45090300+espressom@users.noreply.github.com>
2018-12-07 15:26:25 +0900

    Add files via upload

---
+++ User.java
@@ -0,0 +1,39 @@
+package new_account_manager_6;
+
+public class User {
+	
+	private String name = "";
+	private String id = "";
+	private String pw = "";
+	
+	public User(String name, String id, String pw)
+	{
+		this.name = name;
+		this.id = id;
+		this.pw = pw;
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public String getId()
+	{
+		return id;
+	}
+	
+	public String getPw()
+	{
+		return pw;
+	}
+	
+	public String toString()
+	{
+		String userInfo = "";
+		userInfo += "¿Ã∏ß : " + getName() + "\n";
+		userInfo += "ID : " + getId() + "\n";
+		return userInfo;
+	}
+
+}
